56c82bcafdb3257272cbd251b990b3089409771f
Jorge <jltamayocabello@gmail.com>
2026-05-11 14:38:42 -0400

    Variables

--- ARCHIVOS/main.go
+++ ARCHIVOS/main.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola mundo")
+
+	// VARIABLES
+	// Decalaración explicita
+	var nombre string
+	nombre = "Pedro"
+
+	// Declaración y asignación simultanea
+	var apellido string = "Perez"
+
+	// Declaración implicita :=
+	edad := 30
+
+	fmt.Println("Nombre: ", nombre)
+	fmt.Println("Apellido: ", apellido)
+	fmt.Println("Edad: ", edad)
+
+
 }
 
 // Comentario

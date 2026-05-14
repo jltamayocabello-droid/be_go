6169f660d8c430f5314aa658d1ce78326684acc1
Jorge <jltamayocabello@gmail.com>
2026-05-13 21:41:33 -0400

    Arrays

--- ARCHIVOS/2 Intermedia/Intermedio.go
+++ ARCHIVOS/2 Intermedia/Intermedio.go
@@ -5,6 +5,17 @@
 package main
 import "fmt"
 
-func main(
+func main() { 
 	
-)
+	var nombres [4]string
+	nombres[0] = "Pedro"
+	nombres[1] = "Juan"
+	nombres[2] = "Maria"
+	nombres[3] = "Luis"
+
+
+	apellidos := [4]string{"Perez", "Gomez", "Garcia", "Lopez"}
+	
+	fmt.Println(nombres)
+	fmt.Println(apellidos[0])
+}

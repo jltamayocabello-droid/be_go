54f833a835b23e17f47ed6ae063a479ce265e908
Jorge <jltamayocabello@gmail.com>
2026-05-14 21:18:28 -0400

    Range

--- ARCHIVOS/2 Intermedia/Intermedio.go
+++ ARCHIVOS/2 Intermedia/Intermedio.go
@@ -50,4 +50,13 @@ func main() {
 	}
 	fmt.Println(edades_new["Rimu"])
 
+	// Range
+
+	numeros := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i, num := range numeros {
+		fmt.Println(i, num)
+	}
+
+	
+
 }

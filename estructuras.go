dcb1af21b74e2d956e18f1664830d78b4fb0605b
Jorge <jltamayocabello@gmail.com>
2026-05-15 22:30:44 -0400

    Metodos y receptores - receivers

--- estructuras.go
+++ estructuras.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//Definir una Estructura
 type Book struct{
 	Title string
 	Author string
@@ -9,7 +10,25 @@ type Book struct{
 	Price float64
 }
 
+// Metodo asociado a Book para mostrar detalles del libro
+
+func(b Book) DisplayDetails(){
+	fmt.Println(b.Title)
+	fmt.Println(b.Author)
+	fmt.Println(b.Price)
+	fmt.Println(b.Pages)
+}
+
+// Metodo asociado para aplicar descuentos
+
+func (b *Book) ApplyDiscount(discount float64){
+	b.Price = b.Price - discount
+}
+
 func main() { 
+	
+	// Crear instancia
+
 	myBook := Book{
 		Title: "Programación con GO",
 		Author: "Pedro",
@@ -17,8 +36,12 @@ func main() {
 		Price: 100.00,
 	}
 
-	fmt.Println(myBook.Author)
-	fmt.Println(myBook.Title)
-	fmt.Println(myBook.Price)
-	fmt.Println(myBook.Pages)
+	fmt.Println("---Detalles del libro---")
+	myBook.DisplayDetails()
+
+	myBook.ApplyDiscount(30)
+
+	fmt.Println("---Detalles del libro con el descuento---")
+	myBook.DisplayDetails()
+
 }

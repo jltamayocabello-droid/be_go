dd064a8c832226edf12b2407fc8c3c1d2e398aa0
Jorge <jltamayocabello@gmail.com>
2026-05-20 17:57:22 -0400

    Se crea y escribe un archivo

--- ARCHIVOS/j-archivos/archivos.go
+++ ARCHIVOS/j-archivos/archivos.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 	// Nombre del archivo que vamos a leer
+
 	fileName := "ARCHIVOS/j-archivos/ejemplo.txt"
 
 	content, err := ioutil.ReadFile(fileName) // Leer el archivo
@@ -15,4 +17,23 @@ func main() {
 		return
 	}
 	fmt.Println("Contenido del archivo: ", string(content))
+
+	// Escribir en un archivo
+
+	fileName = "ARCHIVOS/j-archivos/test.txt"
+	text := "Esto es una prueba de escritura en un archivo"
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error al crear el archivo: ", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(text)
+	if err != nil {
+		fmt.Println("Error al escribir en el archivo: ", err)
+		return
+	}
+	fmt.Println("Archivo creado con exito")
 }

acbd4b14e4e59e7baf38af34ab16cabba2a18f1f
Jorge <jltamayocabello@gmail.com>
2026-05-20 17:46:19 -0400

    Se añade comentario leer el archivo

--- ARCHIVOS/j-archivos/archivos.go
+++ ARCHIVOS/j-archivos/archivos.go
@@ -9,7 +9,7 @@ func main() {
 	// Nombre del archivo que vamos a leer
 	fileName := "ARCHIVOS/j-archivos/ejemplo.txt"
 
-	content, err := ioutil.ReadFile(fileName)
+	content, err := ioutil.ReadFile(fileName) // Leer el archivo
 	if err != nil {
 		fmt.Println("Error al leer el archivo: ", err)
 		return

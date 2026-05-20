44d46ffcbf28eb11184660e9d071e66f91efcba4
Jorge <jltamayocabello@gmail.com>
2026-05-19 22:35:34 -0400

    Combinar canales

--- ARCHIVOS/7 Concurrencia/concurrencia.go
+++ ARCHIVOS/7 Concurrencia/concurrencia.go
@@ -100,4 +100,36 @@ func main() {
 				fmt.Println(msg)
 		}
 	}
+
+	// Combinar canales
+
+	func combinar(canal,1, canal2 <- chan string) <-chan string {
+		salida := make(chan string)
+
+		go func() {
+			for mensajeCombinar:= range canal1 {
+				salida <- mensajeCombinar
+			}
+		}()
+
+		go func() {
+			for mensajeCombinar:= range canal2 {
+				salida <- mensajeCombinar
+			}
+		}()
+		return salida
+	}
+			}
+			func main() {
+				canal1 := make(chan string)
+				canal2 := make(chan string)
+				canalCombinado := combinar(canal1, canal2)
+				for mensaje := range canalCombinado {
+					fmt.Println(mensaje)
+		}
+	}
+	canalCombinardo := combinar(canal1, canal2)
+	for mensaje := range canalCombinado {
+		fmt.Println(mensaje)
+	}
 }

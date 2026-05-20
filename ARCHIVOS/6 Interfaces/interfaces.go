d17c5ca2fa4f3986305a6d06b5440de0d1a0652b
Jorge <jltamayocabello@gmail.com>
2026-05-19 21:46:36 -0400

    Polimorfismo

--- ARCHIVOS/6 Interfaces/interfaces.go
+++ ARCHIVOS/6 Interfaces/interfaces.go
@@ -16,8 +16,18 @@ func (c Circle) area() float64 {
 	return 3.1416 * c.radius * c.radius
 }
 
+type Rectangle struct {
+	width, height float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
 func main() {
 
 	c := Circle{10}
+	rectangle := Rectangle{10, 20}
 	fmt.Println("El area de la circunferencia es: ", c.area())
+	fmt.Println("El area del rectangulo es: ", rectangle.area())
 }

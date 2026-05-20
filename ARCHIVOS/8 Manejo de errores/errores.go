e83556a00b89030aaec64f7729b7e01f26c7f954
Jorge <jltamayocabello@gmail.com>
2026-05-20 16:57:12 -0400

    Error personalizado en estructura

--- ARCHIVOS/8 Manejo de errores/errores.go
+++ ARCHIVOS/8 Manejo de errores/errores.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "errors"
 
+// Ejemplo de error en división
 func dividir(a, b float64) (float64, error) {
 	if b == 0 {
 		return 0, errors.New("No se puede dividir por cero")
@@ -10,8 +11,36 @@ func dividir(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
+// Estructura de error personalizada
+
+type DivisionError struct {
+	Dividendo float64
+	Divisor  float64
+}
+
+func(e *DivisionError) Error() string {
+	return fmt.Sprintf("No se puede dividir por cero: %v / %v", e.Dividendo, e.Divisor)
+}
+
+func dividir2(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, &DivisionError{a, b}
+	}
+	return a / b, nil
+}
+
 func main() {
 	resultado, err := dividir(10, 0) // Intento de división con error
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	} else {
+		fmt.Println("El resultado es: ", resultado)
+	}
+
+	// Error Personalizado
+
+	resultado, err = dividir2(10, 0) // Intento de división con error
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return

d0df1e548942dc8373428192c00b50901b7720fe
Jorge <jltamayocabello@gmail.com>
2026-05-21 14:50:59 -0400

    Se comenta código

--- ARCHIVOS/k-json/json.go
+++ ARCHIVOS/k-json/json.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
+	"encoding/json" // Para trabajar con JSON
+	"fmt" // Para imprimir en la consola
+	"os" // Para trabajar con archivos
 )
 
+// Estructura de persona (modela el JSON)
 type Person struct {
 	Name string `json:"nombre"`
 	Age  int    `json:"edad"`
 }
 
+// Función principal
 func main() {
-	fileName := "ARCHIVOS/k-json/person.json"
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("Error al abrir el archivo: ", err)
-		return
+	fileName := "ARCHIVOS/k-json/person.json" // Nombre del archivo (JSON)
+	file, err := os.Open(fileName) // Abrir el archivo (JSON)
+	if err != nil { // Si hay un error
+		fmt.Println("Error al abrir el archivo: ", err) // Imprimir el error
+		return // Salir
 	}
-	defer file.Close()
+	defer file.Close() // Cerrar el archivo
 
-	var persona Person
+	var persona Person // Estructura de persona
 	
-	err = json.NewDecoder(file).Decode(&persona)
+	err = json.NewDecoder(file).Decode(&persona) // Decodificar el archivo en la estructura de persona
+	if err != nil { // Si hay un error
+		fmt.Println("Error al decodificar el archivo: ", err) // Imprimir el error
+		return // Salir
+	}
 	
-	fmt.Println("El nombre es: ", persona.Name)
-	fmt.Println("La edad es: ", persona.Age)
+	fmt.Println("El nombre es: ", persona.Name) // Imprimir el nombre
+	fmt.Println("La edad es: ", persona.Age) // Imprimir la edad
 }
